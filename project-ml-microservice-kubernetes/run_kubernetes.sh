cd5e1ed6e4569f32034f003eb545d9ca6f68af95
EC2 Default User <ec2-user@ip-172-31-3-201.ec2.internal>
2023-04-09 05:50:40 +0000

    submit for assessment

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=tam892007/ml-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-prediction --image="$dockerpath:latest"
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deploy/ml-prediction --address 0.0.0.0 8000:80
 
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=tam892007/ml-prediction
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ml-prediction "$dockerpath:latest"
 
 # Step 3:
 # Push image to a docker repository
+docker push tam892007/ml-prediction:latest
